a477037c92adc8a20ba6d011a4f81f13696d022f
xepaul <edwardpaul44@gmail.com>
2014-10-08 01:24:02 +0100

    Added testing support and some f# implementations of operators

--- tests/ObservableSpecs.fs
+++ tests/ObservableSpecs.fs
@@ -6,6 +6,9 @@ open FSharp.Control.Reactive
 open Builders
 open NUnit.Framework
 
+open Microsoft.Reactive.Testing
+//open FSharp.Reactive.Tests.ReactiveTesting
+open FSharp.Reactive.Tests.ReactiveTesting
 let ``should be`` expectedNext expectedError expectedCompleted (observable:'a IObservable) =
     let next = ref 0
     let error = ref false
@@ -108,3 +111,388 @@ let ``Zipping two observable sequences of different types creates a single zippe
     let expected = ( 1, "A" )
 
     Assert.That(result, Is.EqualTo expected)
+
+
+[<Test>]
+let ``Select Select1``() =
+            let scheduler = new TestScheduler();
+
+            let xs =  [| RxRecording(180L, RxNext 5)
+                         RxRecording(210L, RxNext 4)
+                         RxRecording(240L, RxNext 3)
+                         RxRecording(290L, RxNext 2)
+                         RxRecording(350L, RxNext 1)
+                         RxRecording(400L, RxCompleted)
+                          |] |> scheduler.createHotObservable  
+                                                       
+            let res = scheduler.start  (fun () -> 
+                    xs |> Observable.map(fun x -> x + 1) |> Observable.map( fun x -> x - 2) )
+
+            res .Messages.AssertEqual( //todo remove extension
+                  RxRecording(210L, RxNext (4 + 1 - 2)),
+                  RxRecording(240L, RxNext (3 + 1 - 2)),
+                  RxRecording(290L, RxNext (2 + 1 - 2)),
+                  RxRecording(350L, RxNext (1 + 1 - 2)),
+                  RxRecording(400L, RxCompleted) )
+           
+            xs.Subscriptions.AssertEqual( // to do remove extension method
+                CompletedSubscription(200L,400L)
+            );
+
+
+[<Test>]
+let ``Select Select1-2``() = 
+    let scheduler = new TestScheduler()
+    
+    let xs = 
+        [| RxRecording(180L, RxNext 5)
+           RxRecording(210L, RxNext 4)
+           RxRecording(240L, RxNext 3)
+           RxRecording(290L, RxNext 2)
+           RxRecording(350L, RxNext 1)
+           RxRecording(400L, RxCompleted) |]
+        |> scheduler.createHotObservable
+    
+    let res =  scheduler.controlledStart (fun () -> 
+        xs
+        |> Observable.map (fun x -> x + 1)
+        |> Observable.map (fun x -> x - 2))
+
+    res.Messages.AssertEqual( //todo remove extension
+                              RxRecording(210L, RxNext(4 + 1 - 2)), 
+                              RxRecording(240L, RxNext(3 + 1 - 2)), 
+                              RxRecording(290L, RxNext(2 + 1 - 2)), 
+                              RxRecording(350L, RxNext(1 + 1 - 2)), 
+                              RxRecording(400L, RxCompleted))
+    xs.Subscriptions.AssertEqual( CompletedSubscription(200L, 400L)) // to do remove extension method
+
+[<Test>]
+let paused_no_skip() = 
+    let subscription = ref Unchecked.defaultof<_>
+    let scheduler = TestScheduler()
+    let controller = new System.Reactive.Subjects.Subject<_>()
+    let results = scheduler.createObserver()
+    
+    let xs = 
+        scheduler.createHotObservable ([| onNext 150L 1
+                                          onNext 210L 2
+                                          onNext 230L 3
+                                          onNext 301L 4
+                                          onNext 350L 5
+                                          onNext 399L 6
+                                          onCompleted 500L |])
+    scheduler.scheduleAbsolute 200L (fun s -> 
+        subscription := xs
+                        |> Observable.pauseable controller
+                        |> Observable.subscribeObserver results
+        controller.OnNext(true)
+        System.Reactive.Disposables.Disposable.Empty)
+    |> ignore
+    scheduler.scheduleAbsoluteWithState null 205L (fun s x -> 
+        controller.OnNext(false)
+        System.Reactive.Disposables.Disposable.Empty)
+    |> ignore
+    scheduler.scheduleAbsolute 209L (fun sc -> 
+        controller.OnNext(true)
+        System.Reactive.Disposables.Disposable.Empty)
+    |> ignore
+    scheduler.scheduleAbsolute 1000L (fun sch -> 
+        (!subscription).Dispose()
+        System.Reactive.Disposables.Disposable.Empty)
+    |> ignore
+    scheduler.Start()
+    results.Messages.AssertEqual([| onNext 210L 2
+                                    onNext 230L 3
+                                    onNext 301L 4
+                                    onNext 350L 5
+                                    onNext 399L 6
+                                    onCompleted 500L |])
+
+[<Test>]
+let paused_skips() =
+  let subscription = ref Unchecked.defaultof<_>
+
+  let scheduler = new TestScheduler();
+
+  let controller = new System.Reactive.Subjects.Subject<_>();
+
+  let results = scheduler.createObserver();
+
+  let xs = scheduler.createHotObservable( [|
+                                            onNext 150L 1
+                                            onNext 210L 2
+                                            onNext 230L 3
+                                            onNext 301L 4
+                                            onNext 350L 5
+                                            onNext 399L 6
+                                            onCompleted 500L |])
+
+  scheduler.scheduleAbsolute 200L (fun sch ->
+    subscription := xs |> Observable.pauseable controller |> Observable.subscribeObserver results
+    controller.OnNext(true)
+    System.Reactive.Disposables.Disposable.Empty) |> ignore
+
+  scheduler.scheduleAbsolute 300L  (fun sch ->
+    controller.OnNext(false)
+    System.Reactive.Disposables.Disposable.Empty) |> ignore
+
+  scheduler.scheduleAbsolute 400L (fun sch ->
+    controller.OnNext(true);
+    System.Reactive.Disposables.Disposable.Empty) |> ignore
+
+  scheduler.scheduleAbsolute 1000L (fun sch ->
+    (!subscription).Dispose()
+    System.Reactive.Disposables.Disposable.Empty) |> ignore;
+
+  scheduler.Start()
+
+  results.Messages.AssertEqual( [|
+                                    onNext 210L 2
+                                    onNext 230L 3
+                                    onCompleted 500L |])
+
+
+[<Test>]
+let paused_skips2() = 
+    let scheduler = new TestScheduler()
+    
+    let xs = 
+        [| RxRecording(150L, (RxNext 1))
+           RxRecording(210L, (RxNext 2))
+           RxRecording(230L, (RxNext 3))
+           RxRecording(301L, (RxNext 4))
+           RxRecording(350L, (RxNext 5))
+           RxRecording(399L, (RxNext 6))
+           RxRecording(500L, RxCompleted) |]
+        |> scheduler.createHotObservable
+    
+    let cs = 
+        [| RxRecording(201L, (RxNext true))
+           RxRecording(300L, (RxNext false))
+           RxRecording(400L, (RxNext true))
+           RxRecording(100L, RxCompleted) |]
+        |> scheduler.createHotObservable
+    
+    let results = scheduler.controlledStart (fun () -> xs |> Observable.pauseable cs)
+    ([| RxRecording(210L, (RxNext 2))
+        RxRecording(230L, (RxNext 3))
+        RxRecording(500L, RxCompleted) |], results.Messages)
+    ||> Seq.seqEqual
+    |> Assert.IsTrue
+
+[<Test>]
+let OneShotTimer_TimeSpan_Zero() = 
+    let scheduler = new TestScheduler()
+    let res = scheduler.start (fun () -> Observable.timerSpanWithScheduler scheduler (TimeSpan.FromTicks(0L)))
+    res.Messages.AssertEqual([| onNext 201L 0L
+                                onCompleted 201L |])
+
+[<Test>]
+let Where_Where4() = 
+    let scheduler = new TestScheduler()
+    
+    let xs = 
+        [| onNext 110L 1
+           onNext 180L 2
+           onNext 230L 3
+           onNext 270L 4
+           onNext 340L 5
+           onNext 380L 6
+           onNext 390L 7
+           onCompleted 400L |]
+        |> scheduler.createHotObservable
+    
+    let res = 
+        scheduler.start (fun () -> 
+            xs
+            |> Observable.filteri (fun x i -> i >= 1)
+            |> Observable.filteri (fun x i -> i < 2))
+    
+    res.Messages
+    |> Seq.seqEqual [| onNext 270L 4
+                       onNext 340L 5
+                       onCompleted 400L |]
+    |> Assert.IsTrue
+    res.Messages
+    |> Seq.seqEqual [| onNext 270L 4
+                       onNext 340L 5
+                       onCompleted 400L |]
+    |> Assert.IsTrue
+    xs.Subscriptions.AssertEqual(CompletedSubscription(200L, 400L))
+
+[<Test>]
+let Where_Where42() = 
+    let scheduler = new TestScheduler()
+    
+    let test a (b:RxMessage<_>) = RxNext(b)
+
+
+    let z = test 0 (RxNext  4)
+
+    let xs = 
+        [| RxRecording(110L, (RxNext 1))
+           RxRecording(180L, (RxNext 2))
+           RxRecording(230L, (RxNext 3))
+           RxRecording(270L, (RxNext 4))
+           RxRecording(340L, (RxNext 5))
+           RxRecording(380L, (RxNext 6))
+           RxRecording(390L, (RxNext 7))
+           RxRecording(400L, RxCompleted) |]
+        |> scheduler.createHotObservable
+    
+    let res = 
+        scheduler.start (fun () -> 
+            xs
+            |> Observable.filteri (fun x i -> i >= 1)
+            |> Observable.filteri (fun x i -> i < 2))
+    
+    res.Messages
+    |> Seq.seqEqual [| RxRecording( 270L, (RxNext  4))
+                       RxRecording( 340L, (RxNext  5))
+                       RxRecording( 400L , RxCompleted)|]
+    |> Assert.IsTrue
+    res.Messages
+    |> Seq.seqEqual [| RxRecording(  270L, (RxNext  4))
+                       RxRecording(  340L, (RxNext  5))
+                       RxRecording(  400L, RxCompleted)|]
+    |> Assert.IsTrue
+    xs.Subscriptions.AssertEqual(CompletedSubscription(200L, 400L))
+
+
+//[<Test>]
+//let Select_SelectorThrows() =
+//
+//    let scheduler = new TestScheduler()
+//
+//    let invoked = ref 0
+//
+//    let xs = [|
+//                                            OnNext(180L, 1)
+//                                            //OnError(230L, new Exception())
+//                                            OnNext(210L, 2)
+//                                            
+//                                            OnNext(240L, 3)
+//                                            OnNext(290L, 4)
+//                                            OnNext(350L, 5)
+//                                            OnCompleted(400L)
+//                                            OnNext(410L, -1)
+//                                            OnCompleted(420L)
+//                                            OnError(430L, new Exception()) |]  |> scheduler.createHotObservable
+//    
+//
+//    let ex = new Exception();
+//
+////    let res = scheduler.start (fun () -> xs |> Observable.map (fun x -> invoked := (!invoked) + 1
+////                                                                        if (!invoked = 3) then
+////                                                                           raise (ex)
+////                                                                        x + 1 ))
+//    let doneFinally = ref false
+//    let res = scheduler.start (fun () -> xs |> Observable.finallyDo (fun () -> doneFinally := true)
+//                                            |> ReactiveTesting.fsMap (fun x ->  invoked := (!invoked) + 1
+//                                                                                if (!invoked = 3) then
+//                                                                                     raise (ex)
+//                                                                                x + 1 ))
+//
+//    
+//
+//    res.Messages
+//    |> Seq.seqEqual
+//        [|
+//        OnNext(210L, 3)
+//        OnNext(240L, 4)
+//        OnError(290L, ex) |]
+//    |> Assert.IsTrue
+
+//    Assert.IsTrue (!doneFinally)
+//
+//    xs.Subscriptions.AssertEqual(        CompletedSubscription(200L, 290L)    );
+//
+//    Assert.AreEqual(3, !invoked);
+
+
+
+[<Test>]
+let Select_SelectorThrowsCS() =
+
+    let scheduler = new TestScheduler()
+
+    let invoked = ref 0
+
+    let xs = [|
+                                            OnNext(180L, 1)
+                                            OnNext(210L, 2)
+                                            OnNext(240L, 3)
+                                            OnNext(290L, 4)
+                                            OnNext(350L, 5)
+                                            OnCompleted(400L)
+                                            OnNext(410L, -1)
+                                            OnCompleted(420L)
+                                            OnError(430L, new Exception()) |]  |> scheduler.createHotObservable
+    
+
+    let ex = new Exception();
+
+    let res = scheduler.start (fun () -> xs |> Observable.map (fun x -> invoked := (!invoked) + 1
+                                                                        if (!invoked = 3) then
+                                                                           raise (ex)
+                                                                        x + 1 ))
+//    let res = scheduler.start (fun () -> xs |> ReactiveTesting.fsMap (fun x ->  invoked := (!invoked) + 1
+//                                                                                if (!invoked = 3) then
+//                                                                                     raise (ex)
+//                                                                                x + 1 ))
+
+    
+
+    res.Messages
+    |> Seq.seqEqual
+        [|
+        OnNext(210L, 3)
+        OnNext(240L, 4)
+        OnError(290L, ex) |]
+    |> Assert.IsTrue
+
+    xs.Subscriptions.AssertEqual(        CompletedSubscription(200L, 290L)    );
+
+    Assert.AreEqual(3, !invoked);
+
+[<Test>]
+let SelectWithIndex_Completed() =
+        
+            let scheduler = new TestScheduler();
+
+            let invoked = ref 0;
+
+            let xs = scheduler.createHotObservable([|
+                                                        OnNext(180L, 5)
+                                                        OnNext(210L, 4)
+                                                        OnNext(240L, 3)
+                                                        OnNext(290L, 2)
+                                                        OnNext(350L, 1)
+                                                        OnCompleted(400L)
+                                                        OnNext(410L, -1)
+                                                        OnCompleted(420L)
+                                                        OnError(430L, new System.Exception()) |] )
+            
+
+            let res = scheduler.start( fun () ->
+                xs |> Observable.mapi (fun index x ->
+                
+                    incr invoked
+                    (x + 1) + (index * 10)
+                )
+            )
+
+            res.Messages |> Seq.seqEqual [|
+                                            OnNext(210L, 5)
+                                            OnNext(240L, 14)
+                                            OnNext(290L, 23)
+                                            OnNext(350L, 32)
+                                            OnCompleted(400L) |]
+            |> Assert.IsTrue
+
+            xs.Subscriptions.AssertEqual(
+                CompletedSubscription(200L, 400L)
+            );
+
+            Assert.AreEqual(4, !invoked);
+        
---
+++ tests/ReactiveTesting.fs
@@ -0,0 +1,198 @@
+﻿namespace FSharp.Reactive.Tests
+
+
+/// This is the F# equiv of Microsoft.Reactive.Testing and should be move into a new project
+module ReactiveTesting = 
+    open Microsoft.Reactive.Testing
+    open FSharp.Control.Reactive
+    type RxMessage<'a> = 
+        | RxNext of 'a
+        | RxError of System.Exception
+        | RxCompleted
+    
+    type RxRecordedMessage<'a> = 
+        | RxRecording of time : int64 * message : RxMessage<'a>
+    
+    let onNext t v = RxRecording(t, RxNext v)
+    let onError t v = RxRecording(t, RxError v)
+    let onCompleted t = RxRecording(t, RxCompleted)
+
+    let OnNext(t, v) = RxRecording(t, RxNext v)
+    let OnError(t,v) = RxRecording(t, RxError v)
+    let OnCompleted(t) = RxRecording(t, RxCompleted)
+
+//    let OnNext(t,v) = Recorded<_>(t, System.Reactive.Notification.CreateOnNext<_>(v))
+//    let OnError(t,v) = Recorded<_>(t, System.Reactive.Notification.CreateOnError<_>(v))
+//    let OnCompleted(t) = Recorded<_>(t, System.Reactive.Notification.CreateOnCompleted<_>())
+    
+    type RxPredicatedRecordedMessage<'a when 'a : equality> = 
+        | TestedRecording of recording : RxRecordedMessage<'a>
+        | TesterRecording of recording : RxRecordedMessage<'a>
+        | RxOnNextPredicatedRecording of time : int64 * predicate : ('a -> bool)
+        | RxOnErrorPredicatedRecording of time : int64 * predicate : (System.Exception -> bool)
+        
+        override x.Equals(y) = 
+            // eg to handle predicate ex is timeoutException etc
+            match y with
+            | :? RxPredicatedRecordedMessage<'a> as r -> 
+                match (x, r) with
+                | (TestedRecording(lm), TesterRecording(rm)) -> lm = rm
+                | (TestedRecording(RxRecording(lt, RxNext lv)), RxOnNextPredicatedRecording(rt, rp)) -> lt = rt && rp lv
+                | (TestedRecording(RxRecording(lt, RxError lv)), RxOnErrorPredicatedRecording(rt, rp)) -> lt = rt && rp lv
+                | _ -> false
+            | _ -> false
+        
+        override x.GetHashCode() = 0 //todo
+    
+    type RxSuscription = 
+        | CompletedSubscription of startTime : int64 * endTime : int64
+        | RunningSubscription of startTime : int64
+    
+    type ITestableRxObservable<'a> = 
+        inherit System.IObservable<'a>
+        abstract Messages : ResizeArray<RxRecordedMessage<'a>>
+        abstract Subscriptions : ResizeArray<RxSuscription>
+    
+    type private ColdObservable<'a>(scheduler : TestScheduler, messages : RxRecordedMessage<'a> seq) = 
+        let messages = ResizeArray<_>(messages)
+        let subscriptions = ResizeArray<_>()
+        
+        interface ITestableRxObservable<'a> with
+            member x.Messages = messages
+            member x.Subscriptions = subscriptions
+        
+        interface System.IObservable<'a> with
+            member x.Subscribe observer = 
+                let subscriptionIndex = subscriptions.Count
+                let subscriptionTime = scheduler.Clock
+                subscriptions.Add(RunningSubscription(subscriptionTime))
+                let d = 
+                    messages
+                    |> Seq.map (fun (RxRecording(t, m)) -> 
+                           scheduler.ScheduleRelative(null, t, 
+                                                      System.Func<_, _, _>(fun s t -> 
+                                                          match m with
+                                                          | RxNext(value) -> observer.OnNext value
+                                                          | RxError(value) -> observer.OnError value
+                                                          | RxCompleted -> observer.OnCompleted()
+                                                          System.Reactive.Disposables.Disposable.Empty)))
+                    |> Seq.toList
+                { new System.IDisposable with
+                      member x.Dispose() = 
+                          subscriptions.[subscriptionIndex] <- CompletedSubscription(subscriptionTime, scheduler.Clock)
+                          d |> List.iter (fun d -> d.Dispose()) }
+    
+    type private HotObservable<'a>(scheduler : TestScheduler, messages : RxRecordedMessage<'a> seq) = 
+        let observers = ResizeArray<System.IObserver<'a>>()
+        let messages = ResizeArray<_>(messages)
+        let subscriptions = ResizeArray<_>()
+        
+        do 
+            messages |> Seq.iter (fun (RxRecording(t, m)) -> 
+                            scheduler.ScheduleRelative(null, t, 
+                                                       System.Func<_, _, _>(fun s t -> 
+                                                           observers
+                                                           |> Seq.toArray
+                                                           |> Seq.iter (fun observer -> 
+                                                                  match m with
+                                                                  | RxNext(value) -> observer.OnNext value
+                                                                  | RxError(value) -> observer.OnError value
+                                                                  | RxCompleted -> observer.OnCompleted())
+                                                           System.Reactive.Disposables.Disposable.Empty))
+                            |> ignore)
+        
+        interface ITestableRxObservable<'a> with
+            member x.Messages = messages
+            member x.Subscriptions = subscriptions
+        
+        interface System.IObservable<'a> with
+            member x.Subscribe observer = 
+                let subscriptionIndex = subscriptions.Count
+                let subscriptionTime = scheduler.Clock
+                observers.Add(observer)
+                subscriptions.Add(RunningSubscription(subscriptionTime))
+                { new System.IDisposable with
+                      member x.Dispose() = 
+                          observers.Remove(observer) |> ignore
+                          subscriptions.[subscriptionIndex] <- CompletedSubscription(subscriptionTime, scheduler.Clock) }
+    
+    type ITestableObserver<'a> = 
+        inherit System.IObserver<'a>
+        abstract Messages : System.Collections.Generic.IList<RxRecordedMessage<'a>>
+    
+    type MockObserver<'a>(scheduler : TestScheduler) = 
+        
+        do 
+            if (scheduler = null) then failwith "scheduler null"
+        
+        let messages = ResizeArray<RxRecordedMessage<'a>>()
+        interface ITestableObserver<'a> with
+            member x.Messages = messages :> System.Collections.Generic.IList<_>
+            member x.OnNext v = messages.Add(RxRecording(scheduler.Clock, RxNext v))
+            member x.OnError v = messages.Add(RxRecording(scheduler.Clock, RxError v))
+            member x.OnCompleted() = messages.Add(RxRecording(scheduler.Clock, RxCompleted))
+    
+    type Microsoft.Reactive.Testing.TestScheduler with
+        member x.createColdObservable stream = new ColdObservable<_>(x, stream) :> ITestableRxObservable<_>
+        member x.createHotObservable stream = new HotObservable<_>(x, stream) :> ITestableRxObservable<_>
+        member x.scheduleAbsoluteWithState<'a> state dueTime f = x.ScheduleAbsolute<'a>(state, dueTime, System.Func<_, _, _> f)
+        member x.scheduleAbsolute dueTime f = x.ScheduleAbsolute<obj>(null, dueTime, System.Func<_, _, _>(fun sc s -> f (sc)))
+        member x.createObserver<'a>() = MockObserver<'a>(x) :> ITestableObserver<_>
+        
+        member x.startWithAll (create : unit -> System.IObservable<'a>) created subscribed disposed = 
+            //guard
+            let source : System.IObservable<'a> ref = ref null
+            let subscription : System.IDisposable option ref = ref None
+            let observer = x.createObserver<'a>()
+            x.ScheduleAbsolute(null, created, 
+                               System.Func<_, _, _>(fun scheduler s -> 
+                                   source := create()
+                                   System.Reactive.Disposables.Disposable.Empty))
+            |> ignore
+            x.ScheduleAbsolute(null, subscribed, 
+                               System.Func<_, _, _>(fun scheduler s -> 
+                                   subscription := Some((!source) |> Observable.subscribeObserver observer)
+                                   System.Reactive.Disposables.Disposable.Empty))
+            |> ignore
+            x.ScheduleAbsolute(null, disposed, 
+                               System.Func<_, _, _>(fun scheduler s -> 
+                                   (!subscription) |> Option.iter (fun v -> v.Dispose())
+                                   System.Reactive.Disposables.Disposable.Empty))
+            |> ignore
+            x.Start()
+            observer
+        
+        member x.startWithDispose create disposed = 
+            //guard
+            x.startWithAll create ReactiveTest.Created ReactiveTest.Subscribed disposed
+        member x.start create = 
+            //guard
+            x.startWithAll create ReactiveTest.Created ReactiveTest.Subscribed ReactiveTest.Disposed
+        
+        member x.controlledStartWithAll created subscribed disposed create = 
+            let results = x.createObserver()
+            Observable.timerSpanWithScheduler x (System.TimeSpan.FromTicks created)
+            |> Observable.flatmap (fun _ -> create())
+            |> Observable.delaySubscriptionWithScheduler x (System.TimeSpan.FromTicks(subscribed - created))
+            |> Observable.takeUntilOther (Observable.timerSpanWithScheduler x (System.TimeSpan.FromTicks disposed))
+            |> Observable.subscribeObserver results
+            |> ignore
+            x.Start()
+            results
+        
+        member x.controlledStartWithDisposed disposed create = x.controlledStartWithAll ReactiveTest.Created ReactiveTest.Subscribed disposed create
+        member x.controlledStart create = x.controlledStartWithAll ReactiveTest.Created ReactiveTest.Subscribed ReactiveTest.Disposed create
+
+module Seq = 
+    let seqEqual (s1 : 'a seq when 'a : equality) (s2 : 'a seq when 'a : equality) = 
+        use e1 = s1.GetEnumerator()
+        use e2 = s1.GetEnumerator()
+        let nb = ref true
+        let r = ref true
+        while ((!nb) && e1.MoveNext()) do
+            if (e2.MoveNext() = false || e1.Current <> e2.Current) then 
+                r := false
+                nb := false
+        if ((!nb) && e2.MoveNext()) then r := false
+        !r
+//  let seqEqual (s1: 'a seq when 'a : equality) (s2: 'a seq when 'a : equality) = (Seq.compareWith Operators.compare s1 s2) =0
