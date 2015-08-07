cc6e0b9c172947d1b751775c61a299d154876563
Jared Hester <jared.hester@gmail.com>
2015-08-07 17:39:04 -0400

    Updated packages

---
+++ src/assemblyinfo/assemblyinfo.shared.fs
@@ -0,0 +1,10 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyVersionAttribute("3.3.0")>]
+[<assembly: AssemblyFileVersionAttribute("3.3.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("{\"buildVersion\":\"3.3.0\",\"buildDate\":\"2015-08-05T21:42:10-04:00\"}")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "3.3.0"
