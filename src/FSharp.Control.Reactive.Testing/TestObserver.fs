da13895a4856b3de5ef9be8b80c3c47a7500b09e
Stijn Moreels <stijn.moreels@codit.eu>
2018-06-05 12:36:36 +0200

    Adds test observer

--- src/FSharp.Control.Reactive.Testing/TestObserver.fs
+++ src/FSharp.Control.Reactive.Testing/TestObserver.fs
@@ -2,10 +2,16 @@
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module FSharp.Control.Reactive.Testing.TestObserver
 
+open System
 open Microsoft.Reactive.Testing
 
 /// Gets recorded timestamped notification messages received by the observer.
+[<Obsolete("Use 'messages' to get all the recored timestamped notifications")>]
 let all (o : ITestableObserver<'a>) = 
+    o.Messages |> Seq.toList |> TestNotifications
+
+/// Gets recorded timestamped notification messages received by the observer.
+let messages (o : ITestableObserver<'a>) =
     o.Messages |> Seq.toList |> TestNotifications
 
 /// Gets recorded timestamped "OnNext" notification messages received by the observer.
