bf19e66f00de6613f6b537ad22cc2ec9339275d6
Mark Lambert <marklam@spunow.co.uk>
2024-09-24 10:57:28 +0100

    Working on build compatible with nuget publish via github actions

--- build.ps1
+++ build.ps1
@@ -1,22 +1,4 @@
-[xml]$doc = Get-Content .\src\Directory.Build.props
-$version = $doc.Project.PropertyGroup.VersionPrefix # the version under development, update after a release
-$versionSuffix = '-build.0' # manually incremented for local builds
-
-function isVersionTag($tag){
-    $v = New-Object Version
-    [Version]::TryParse($tag, [ref]$v)
-}
-
-if ($env:appveyor){
-    $versionSuffix = '-build.' + $env:appveyor_build_number
-    if ($env:appveyor_repo_tag -eq 'true' -and (isVersionTag($env:appveyor_repo_tag_name))){
-        $version = $env:appveyor_repo_tag_name
-        $versionSuffix = ''
-    }
-    Update-AppveyorBuild -Version "$version$versionSuffix"
-}
-
 dotnet tool restore
-dotnet build -c Release /p:Version=$version$versionSuffix
+dotnet build -c Release
 dotnet test --no-build -c Release tests
-dotnet pack --no-restore -c Release /p:Version=$version$versionSuffix -o $psscriptroot/bin
+dotnet pack --no-restore -c Release -o $psscriptroot/bin
